60b26b654ca97916e561fe748b7fbe98826087fd
fan9704 <cxz123499@gmail.com>
2025-03-03 20:53:33 +0800

    test: Pytest Infrastructure Complete

---
+++ tests/common/__init__.py
@@ -0,0 +1,28 @@
+# import pytest
+# from tortoise import Tortoise
+# from src.configs import tortoise_config
+#
+# @pytest.fixture()
+# async def init_db():
+#     """
+#     初始化 Tortoise ORM (基於原始 `init_db` 設定)
+#     """
+#     config = {
+#         "use_tz": True,
+#         "timezone": "Asia/Taipei",
+#         "connections": {
+#             "default": tortoise_config.db_url
+#         },
+#         "apps": {
+#             "models": {
+#                 "models": ["src.models.tortoise"],
+#                 'default_connection': 'default',
+#             }
+#         }
+#     }
+#
+#     # 手動初始化 Tortoise ORM
+#     await Tortoise.init(config=config)
+#     await Tortoise.generate_schemas()  # 建立表格
+#     yield
+#     await Tortoise.close_connections()  # 測試結束後關閉資料庫
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,31 @@
-"""
-Place something here...
-"""
+import os
+import pytest_asyncio
+from dotenv import load_dotenv
+
+@pytest_asyncio.fixture(scope="session")
+async def dependencies(request):
+    args = request.config
+
+    if args.getoption("prod"):
+        load_dotenv(".env.prod")
+    elif args.getoption("test"):
+        load_dotenv(".env.test")
+    elif args.getoption("dev"):
+        load_dotenv(".env.dev")
+    else:
+        load_dotenv()
+
+    # if args.getoption("sync"):
+    #         os.environ["RUN_MODE"] = "SYNC"
+    # else:
+    #     os.environ["RUN_MODE"] = "ASYNC"
+
+    # os.environ["DB_TYPE"] = args.getoption("db")
+    # print("DB_TYPE",os.getenv("DB_TYPE"))
+
+def pytest_addoption(parser):
+    parser.addoption("--prod",action="store_true", help="Run the server in production mode.")
+    parser.addoption("--test",action="store_true", help="Run the server in test mode.")
+    parser.addoption("--dev",action="store_true", help="Run the server in development mode.")
+    parser.addoption("--sync",action="store_true", help="Run the server in Sync mode.")
+    parser.addoption("--db", help="Run the server in database type.",choices=["mysql","postgresql"], default="postgresql")
