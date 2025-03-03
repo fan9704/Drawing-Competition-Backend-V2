f1d5ef2f94e880ab0efbfd82692869a3d1ef95ce
fan9704 <cxz123499@gmail.com>
2025-03-04 00:27:03 +0800

    test: Test DB Config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,38 @@
+import asyncio
 import os
+
+import pytest
 import pytest_asyncio
 from dotenv import load_dotenv
+from tortoise import Tortoise
+from tortoise.contrib.test import _init_db, getDBConfig
+
+load_dotenv()
+
+# DB Config
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_TEST_DB")
+POSTGRES_DB_URL = f"postgres://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+DB_MODELS = ["src.models.tortoise"]
+
+@pytest.fixture(scope="session")
+def event_loop():
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
+
+@pytest.fixture(scope="session")
+def in_memory_db(request, event_loop):
+    config = getDBConfig(app_label="models", modules=DB_MODELS)
+    event_loop.run_until_complete(_init_db(config))
+    request.addfinalizer(lambda: event_loop.run_until_complete(Tortoise._drop_databases()))
+
 
 @pytest_asyncio.fixture(scope="session")
 async def dependencies(request):
@@ -15,13 +47,13 @@ async def dependencies(request):
     else:
         load_dotenv()
 
-    # if args.getoption("sync"):
-    #         os.environ["RUN_MODE"] = "SYNC"
-    # else:
-    #     os.environ["RUN_MODE"] = "ASYNC"
+    if args.getoption("sync"):
+            os.environ["RUN_MODE"] = "SYNC"
+    else:
+        os.environ["RUN_MODE"] = "ASYNC"
 
-    # os.environ["DB_TYPE"] = args.getoption("db")
-    # print("DB_TYPE",os.getenv("DB_TYPE"))
+    os.environ["DB_TYPE"] = args.getoption("db")
+    print("DB_TYPE",os.getenv("DB_TYPE"))
 
 def pytest_addoption(parser):
     parser.addoption("--prod",action="store_true", help="Run the server in production mode.")
